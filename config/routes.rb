8b1e7dc23b2649b384b76f8baddb73110923d950
Rusty Meadows <rustyameadows@gmail.com>
2019-10-21 12:02:34 -0700

    Add Socery Auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root :to => 'users#index'
+  resources :user_sessions
+  resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
